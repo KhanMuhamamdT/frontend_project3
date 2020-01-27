171dc234acfc475a43a2df21d20ef4698375a6ff
ALDO_M <aldo.marukot@gmail.com>
2020-01-27 18:54:45 -0500

    styling for CreatePost page done

--- src/CreatePost.js
+++ src/CreatePost.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import "./CreatePost.css";
+
 const CreatePost = props => {
   const [title, setTitle] = useState("");
   const [url, setURL] = useState("");
@@ -21,36 +23,60 @@ const CreatePost = props => {
   };
 
   return (
-    <div className="container">
-      <div className="shadow-sm p-3">
+    <div className="container ">
+      <div className="shadow-lg p-3 create-post-container my-3">
         <h1>Create a post...</h1>
 
-        <form onSubmit={props.handleSubmit}>
-          <label htmlform="title">Title:</label>
-          <input
-            className="input"
-            value={title}
-            type="text"
-            onChange={e => handleTitle(e)}
-          ></input>
+        <form onSubmit={props.handleSubmit} className="column">
+          <div className="input-group my-3">
+            <div className="input-group-prepend">
+              {/* <label htmlform="title">Title:</label> */}
+              <span className="input-group-text" htmlform="title">
+                Title:{" "}
+              </span>
+            </div>
+            <input
+              value={title}
+              type="text"
+              className="form-control"
+              onChange={e => handleTitle(e)}
+            ></input>
+          </div>
+          <div className="input-group my-3">
+            <div className="input-group-prepend">
+              {/* <label htmlform="url">URL:</label> */}
+              <span className="input-group-text" htmlform="url">
+                URL:{" "}
+              </span>
+            </div>
+            <input
+              value={url}
+              type="text"
+              className="form-control"
+              onChange={e => handleURL(e)}
+            ></input>
+          </div>
+          <div className="input-group my-3">
+            <div className="input-group-prepend">
+              {/* <label htmlform="Description">Description:</label> */}
+              <span
+                className="input-group-text descr-box"
+                htmlform="description"
+              >
+                Description:{" "}
+              </span>
+            </div>
+            <textarea
+              value={description}
+              type="text"
+              className="form-control"
+              onChange={e => handleDescription(e)}
+            ></textarea>
+          </div>
 
-          <label htmlform="url">URL:</label>
-          <input
-            className="input"
-            value={url}
-            type="text"
-            onChange={e => handleURL(e)}
-          ></input>
-
-          <label htmlform="Description">Description:</label>
-          <input
-            className="input"
-            value={description}
-            type="text"
-            onChange={e => handleDescription(e)}
-          ></input>
-
-          <button className="btn">SUBMIT</button>
+          <button type="button" className="btn btn-success my-3">
+            SUBMIT
+          </button>
         </form>
       </div>
     </div>
