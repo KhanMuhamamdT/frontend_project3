aa422178330b84ab06fb7fdfb058da89f0b945e0
ALDO_M <aldo.marukot@gmail.com>
2020-01-28 15:41:03 -0500

    styling changes to PostDetails

--- src/PostDetails.js
+++ src/PostDetails.js
@@ -1,13 +1,20 @@
 import React from "react";
 
 import "./App.css";
+import "./PostDetails.css";
 
 function PostDetails() {
   return (
     <div className="container">
-      <div className="shadow-lg p-3 my-3">
-        <div>Post Title</div>
-        <div>Photo</div>
+      <div className="custom-post-container shadow-lg p-3 my-3">
+        <h1>Post Title</h1>
+        <div className="custom-post-photo">
+          Photo
+          <img
+            className="custom-post-photo-img"
+            src="https://images.unsplash.com/photo-1497215842964-222b430dc094?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+          />
+        </div>
         <div>Posted By</div>
         <div>Comments</div>
       </div>
